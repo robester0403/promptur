5f29dcafedf1bc45bc1e114c3d67b9f624b03c01
robester0403 <robkso@gmail.com>
2022-11-23 13:36:04 -0500

    Install deps and set initial permissions

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Camera } from "expo-camera";
+import { Video } from "expo-av";
+
+export default function App() {
+  const [hasAudioPermission, setHasAudioPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [record, setRecord] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+
+      const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+      setHasAudioPermission(audioStatus.status === "granted");
+    })();
+  }, {});
+
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
